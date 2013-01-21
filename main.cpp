31a17ac4c74af0732e14d2141ae2b8242fe74dcd
gpltaylor <garrytaylor@fluentmoney.co.uk>
2013-01-21 12:52:21 +0000

    remove new line

--- main.cpp
+++ main.cpp
@@ -14,7 +14,6 @@
    along with this library; see the file COPYING.LIB.  If not, write to
    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
    Boston, MA 02111-1307, USA.
-   
 */
 #include <qapplication.h>
 #include "Regit.h"
